2fea4d5f2ce49985ca39755cda9f3e15c2bceb61
Bartek Pieła <Bartek-Macbook@MacBook-Pro-Bartek.fritz.box>
2016-08-06 20:23:37 +0200

    menu

---
+++ js/default.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.menu-icon').click(function () {
+        $('.menu').slideToggle("fast");
+    });
+
+});
+
+// $(document).ready(function () {
+//     alert("Hello World");
+// });
